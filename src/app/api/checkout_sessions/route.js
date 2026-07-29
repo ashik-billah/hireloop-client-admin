df048ed8e566a0673aa3c02a8bb809609c6b3f4a
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-07-29 21:15:07 +0600

    add server route

--- src/app/api/checkout_sessions/route.js
+++ src/app/api/checkout_sessions/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { headers } from 'next/headers'
-
+//add
 import { stripe } from '../../../lib/stripe'
 import { PLAN_PRICE_ID } from '@/lib/stripe'
 import { getUserSession } from '@/lib/core/session'
