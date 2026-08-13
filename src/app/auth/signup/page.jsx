868342bb3aa7d62926efcae4b04af79f26ddf231
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-08-13 08:54:51 +0600

    error control

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -47,7 +47,7 @@ export default function SignupPage() {
                 role,
                 plan
             });
-
+// add
             if (authError) {
                 setError(authError.message || "Something went wrong during signup.");
             } else {
